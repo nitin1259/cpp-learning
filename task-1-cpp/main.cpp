56b64a6619db4dc56d3292af548f390f0c6f2bd4
Nitin SINGH <nitinkumar.singh@f5.com>
2021-06-15 19:51:08 +0530

    assignment of fundatmental concepts

--- task-1-cpp/main.cpp
+++ task-1-cpp/main.cpp
@@ -156,6 +156,78 @@ But we're not interested in catching it. We just want to make sure that it consu
 
 
 
+    // assignment - average of 3 double numbers 
+
+    double num1;
+    double num2;
+    double num3;
+    double avg;
+
+    cout << "Enter the real number: "<< endl;
+    cin>>num1;
+
+    cout << "Enter the anouther real number: "<< endl;
+    cin>>num2;
+
+    cout << "Enter the anouther real number: "<< endl;
+    cin>>num3;
+
+    avg = (num1+num2+num3)/3;
+
+    cout<< "Average of entered real number is: "<< avg << endl;
+
+
+    // assignment - Madlibs Clone
+    string adjective1;
+	string girlsName;
+	string adjective2;
+	string occupation1;
+	string placeName;
+	string clothing;
+	string hobby;
+	string adjective3;
+	string occupation2;
+	string boysName;
+	string mansName;
+
+	cout << "Please enter an adjective" << endl;
+	getline(cin, adjective1);
+
+	cout << "Please enter a girl's name" << endl;
+	getline(cin, girlsName);
+
+	cout << "Please enter another adjective" << endl;
+	getline(cin, adjective2);
+
+	cout << "Please enter an occupation" << endl;
+	getline(cin, occupation1);
+
+	cout << "Please enter the name of a place" << endl;
+	getline(cin, placeName);
+
+	cout << "Please enter the name of an item of clothing in plural" << endl;
+	getline(cin, clothing);
+
+	cout << "Please enter a hobby" << endl;
+	getline(cin, hobby);
+
+	cout << "Please enter yet another adjective" << endl;
+	getline(cin, adjective3);
+
+	cout << "Please enter another occupation" << endl;
+	getline(cin, occupation2);
+
+	cout << "Please enter a boy's name" << endl;
+	getline(cin, boysName);
+
+	cout << "Please enter a man's name" << endl;
+	getline(cin, mansName);
+
+	//print out the story to the user
+	cout << "There once was a " << adjective1 << " girl named " << girlsName << " who was a " << adjective2
+		<< " " << occupation1 << " in the Kingdom of " << placeName << ".  She loved to wear "
+		<< clothing << " and " << hobby << ".  She wanted to marry the " << adjective3 << " "
+		<< occupation2 << " named " << boysName << " but her father, King " << mansName << " forbid her from seeing him." << endl;
 
     return 0;
 }
